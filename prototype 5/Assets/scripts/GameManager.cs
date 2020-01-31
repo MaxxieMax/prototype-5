8455f69a2911d6ac35c6b17e8098bb3d7a97dcbc
MaxxieMax <Max.breunesse@gmail.com>
2020-01-31 21:38:29 +0100

    prototype 5

---
+++ prototype 5/Assets/scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public bool isGameActive;
+    private int score;
+    private float spawnRate = 1.0f;
+    
+
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //de code voor het spawnen van de objecten
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+
+    }
+    //de code voor de punte telling
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score:" + score;
+    }
+
+    //de codes voor de Game over
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+
+        gameOverText.gameObject.SetActive(true);
+
+        isGameActive = false;
+    }
+
+    // de code voor de restart button
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
+    }
+}
